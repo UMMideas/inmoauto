1dd01c01b2b4e3e1900bbc1084fc864a460d2972
UMMideas <ummideas@gmail.com>
2025-12-16 12:58:58 -0300

    Update check-pro.js

--- api/check-pro.js
+++ api/check-pro.js
@@ -1,10 +1,10 @@
-import { esUsuarioPro } from '../../lib/pro-store';
+import { esUsuarioPro } from '../lib/pro-store';
 
 export default function handler(req, res) {
   const { email } = req.body;
 
-  const isPro = esUsuarioPro(email);
-
-  res.json({ pro: isPro });
+  res.json({
+    pro: esUsuarioPro(email)
+  });
 }
 
